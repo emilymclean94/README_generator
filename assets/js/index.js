73ccf5f2ae51412e47c11b086d2797d0117271f9
Emily McLean <emilymclean93@gmail.com>
2023-04-19 17:26:59 -0400

    added inquirer prompt() objects

--- assets/js/index.js
+++ assets/js/index.js
@@ -5,6 +5,35 @@ const fs = require('fs');
 // TODO: Create an array of questions for user input
 const questions = [];
 
+inquirer
+.prompt ([
+    {
+        type: 'input',
+        message: 'What is the title of your project?',
+        name: 'title',
+    },
+    {
+        type: 'input',
+        message: 'What is the description of your project?',
+        name: 'description',
+    },
+    {
+        type: 'input',
+        message: 'Please provide instructions and examples of how to use your project',
+        name: 'usage',
+    },
+    {
+        type: 'input',
+        message: 'How can others contribute?',
+        name: 'contribute',
+    },
+    {
+        type: 'input',
+        message: 'Provide examples on how to run the tests written for your application',
+        name: 'tests',
+    },
+])
+
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
