a29c620bcb08a54f20b5f9b26de28e162a23e70a
Emily McLean <emilymclean93@gmail.com>
2023-04-19 22:22:52 -0400

    added pseudo code to generateMardown.js

--- assets/js/generateMarkdown.js
+++ assets/js/generateMarkdown.js
@@ -1,7 +1,11 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  // if license = MIT
+  // if license.value = MIT then return [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+  // else if Apache then return [![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+  // else if --> Add rest of licenses? <--
+  // else return license as an empty string
+
 }
 
 // TODO: Create a function that returns the license link
--- assets/js/index.js
+++ assets/js/index.js
@@ -107,5 +107,4 @@ inquirer
 // TODO: Create a function to initialize app
 function init() { }
 
-// // Function call to initialize app
-// init();
+
